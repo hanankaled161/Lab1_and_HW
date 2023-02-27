91cafa3d56b582245520be24d475cbd80ccfcb48
hanankaled161 <110997076+hanankaled161@users.noreply.github.com>
2023-02-27 20:32:07 +0300

    Add files via upload

---
+++ LAB1H/src/Q5.java
@@ -0,0 +1,13 @@
+public class Q5 {
+    public static void main(String[] args) {
+        int [] numbers={0,1,2,3,4,5,6,7,8,9,10};
+        print(numbers);
+    }
+
+    static void print(int [] array)
+    {
+        for (int i = 0; i <array.length ; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
